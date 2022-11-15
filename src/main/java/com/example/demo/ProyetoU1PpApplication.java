96b5f83be6e7b8cf573f0720db6e190033fe8fc3
Edison Cayambe <edisonc_89@hotmail.com>
2022-11-14 19:26:33 -0500

    comentario

---
+++ src/main/java/com/example/demo/ProyetoU1PpApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyetoU1PpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyetoU1PpApplication.class, args);
+	}
+
+}
